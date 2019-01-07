dbb8118d767d4788f1485c63f82e46094147a4ed
Eric Sturman <eric.sturman@gmail.com>
2019-01-06 19:28:52 -0600

    logistic regression

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -1,16 +1,27 @@
 # Data Preprocessing Template
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[,3:5]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+training_set[,1:2] = scale(training_set[,1:2])
+test_set[,1:2] = scale(test_set[,1:2])
+
+# Fitting Logistic Regression Training Set
+classifier = glm(formula = Purchased ~ Age + EstimatedSalary, family = binomial, data = training_set)
+
+# Predicting the Test set results
+prob_pred = predict(classifier, type = 'response', newdata = test_set[,-3])
+y_pred = round(prob_pred)
+
+# making confusion matrix
+cm = table(test_set[,3], y_pred)
