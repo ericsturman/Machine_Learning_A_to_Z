1b618eec3a1a97adf2c36d8cbf70a64456af5ed5
Eric Sturman <eric.sturman@gmail.com>
2019-01-06 19:38:59 -0600

    ...

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -1,27 +1,16 @@
 # Data Preprocessing Template
 
 # Importing the dataset
-dataset = read.csv('Social_Network_Ads.csv')
-dataset = dataset[,3:5]
+dataset = read.csv('Data.csv')
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[,1:2] = scale(training_set[,1:2])
-test_set[,1:2] = scale(test_set[,1:2])
-
-# Fitting Logistic Regression Training Set
-classifier = glm(formula = Purchased ~ Age + EstimatedSalary, family = binomial, data = training_set)
-
-# Predicting the Test set results
-prob_pred = predict(classifier, type = 'response', newdata = test_set[,-3])
-y_pred = round(prob_pred)
-
-# making confusion matrix
-cm = table(test_set[,3], y_pred)
+# training_set = scale(training_set)
+# test_set = scale(test_set)
